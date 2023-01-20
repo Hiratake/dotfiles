4207b27a7bff565d6809d4b15680fa23e374f456
Hiratake <hiratake@hiratake.xyz>
2023-01-21 08:13:30 +0900

    zshの設定を追加

--- home/run_once_dotfiles.sh
+++ home/run_once_dotfiles.sh
@@ -13,23 +13,6 @@ error() {
     printf -- "%sError: $*%s\n" >&2 "$RED" "$RESET"
 }
 
-setup_dependencies() {
-    printf -- "\n%sSetting up dependencies:%s\n\n" "$BOLD" "$RESET"
-
-    # Install Homebrew
-    if ! command_exists brew; then
-        printf -- "%sInstalling Homebrew...%s\n" "$BLUE" "$RESET"
-        /bin/bash -c \
-            "$(wget -qO- https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-        source "${HOME}/.bash_profile"
-        brew doctor
-        brew upgrade
-    fi
-    printf -- "%sInstalling/updating apps using Homebrew...%s\n" "$BLUE" "$RESET"
-    brew bundle --global
-    brew cleanup
-}
-
 setup_color() {
     if [ -t 1 ]; then
         RED=$(printf '\033[31m')
@@ -48,14 +31,40 @@ setup_color() {
     fi
 }
 
+setup_dependencies() {
+    printf -- "\n%sSetting up dependencies:%s\n\n" "$BOLD" "$RESET"
+
+    # Install Homebrew
+    if ! command_exists brew; then
+        printf -- "%sInstalling Homebrew...%s\n" "$BLUE" "$RESET"
+        /bin/bash -c \
+            "$(wget -qO- https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+        source "${HOME}/.bash_profile"
+        brew doctor
+        brew upgrade
+    fi
+    printf -- "%sInstalling/updating apps using Homebrew...%s\n" "$BLUE" "$RESET"
+    brew bundle --global
+    brew cleanup
+}
+
+setup_shell() {
+    printf -- "\n%sSetting up shell frameworks:%s\n\n" "$BOLD" "$RESET"
+
+    # zsh
+    command -v zsh | sudo tee -a /etc/shells
+    sudo chsh -s "$(command -v zsh)" "${USER}"
+}
+
 main() {
+    setup_color
     printf -- "\n%sdotfiles setup script%s\n" "$BOLD" "$RESET"
 
     sudo apt update && sudo apt install -y build-essential procps curl file git
     source "${HOME}/.bash_profile"
 
     setup_dependencies
-    setup_color
+    setup_shell
 
     printf -- "\n%sDone.%s\n\n" "$GREEN" "$RESET"
 }
